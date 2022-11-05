8ece744121c61909ae1cc5beea1ab4228b4809ca
John Conrad <johnconrad314@gmail.com>
2022-11-05 00:55:19 -0500

    handle divisor set to 0

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,7 +1,6 @@
-﻿// Declare variables and then initialize to zero -- who wrote this, they're ints... they'll instantiate to 0 the moment we declare them since that's the default value
-// can't critique too much... I am still doing this tutorial
-int num1 = 0;
-int num2 = 0;
+﻿// Declare variables and then initialize to zero
+double num1 = 0;
+double num2 = 0;
 
 // Display title as the C# console calculator app.
 Console.WriteLine("Console Calculator in C#\r");
@@ -9,11 +8,11 @@ Console.WriteLine("------------------------\n");
 
 // Ask the user to type the first number...
 Console.WriteLine("Type a number, and then press Enter");
-num1 = Convert.ToInt32(Console.ReadLine());
+num1 = Convert.ToDouble(Console.ReadLine());
 
 // Ask the user to type the second number...
 Console.WriteLine("Type another number, and then press Enter");
-num2 = Convert.ToInt32(Console.ReadLine());
+num2 = Convert.ToDouble(Console.ReadLine());
 
 // Ask the user to chose an option...
 Console.WriteLine("Choose an option from the following list:");
@@ -36,6 +35,11 @@ switch(Console.ReadLine())
         Console.WriteLine($"Your result: {num1} * {num2} = {num1 * num2}");
         break;
     case "d":
+        while (num2 == 0)
+        {
+            Console.WriteLine("Enter a non-zero divisor: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+        }
         Console.WriteLine($"Your result: {num1} / {num2} = {num1 / num2}");
         break;
 }
